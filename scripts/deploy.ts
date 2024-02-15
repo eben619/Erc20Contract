51f7d5c8dbeab133dd54235040f76f101c51ca2a
eben619 <120148252+eben619@users.noreply.github.com>
2024-02-15 14:49:41 +0000

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  
+  console.log("Deploying Erc20contract contract...");
+  const Erc20contract = await ethers.getContractFactory("Erc20contract");
+  const deployedErc20contract = await Erc20contract.deploy(
+    "_name",
+    "_symbol", 
+    "_decimals",
+    ethers.utils.parseEther("100000") 
   );
+  await deployedErc20contract.deployed();
+
+  console.log("Erc20contract deployed to:", deployedErc20contract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
